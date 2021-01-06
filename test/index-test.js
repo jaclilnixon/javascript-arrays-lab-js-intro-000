96a7d7e2f085f266d1fc920af7dc0fb9eaac76ca
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-06 20:15:04 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,7 +5,7 @@ describe('Arrays', function() {
     })
   })
 
-  var kittens = ['Milo', 'Otis', 'Garfield'];
+
 
   describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
@@ -15,10 +15,7 @@ describe('Arrays', function() {
     })
   })
 
-function destructivelyAppendKitten(name) {
-  kittens.push(name)
-  return kittens
-}
+
 
 
   describe('destructivelyPrependKitten(name)', function() {
