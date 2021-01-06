a6b0afbb74c280118e67aeaadb6bf314d8586ca2
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-06 20:16:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
- var kittens = [ 'Milo', 'Otis', 'Garfield' ]//define your array here
+ var kittens = [ 'Milo', 'Otis', 'Garfield' ]
+ //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+   var kittens = [ 'Milo', 'Otis', 'Garfield' ]
+   kittens.push(name)
+   return kittens
+}
