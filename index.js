713e4460a78a35ea4a27e0969c2d51c6008931d0
Jacqueline Nixon <jacquelinenixon@me.com>
2021-01-06 20:19:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten() {
-   var kittens = [ 'Milo', 'Otis', 'Garfield' ]
-   kittens.push()
-   return kittens
+function destructivelyAppendKitten(name) {
+   var newArray = [ 'Milo', 'Otis', 'Garfield' ]
+   newArray.push(name)
+   return newArray
 }
